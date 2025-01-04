a03cb685cf2c0a7f435984dfffc763c835b14974
truckdrivercz <eda.orvos@seznam.cz>
2025-01-04 13:40:53 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,22 @@
 let score = 0;
+let upgradeCost = 10;
+let cookiesPerClick = 1;
 
 const cookie = document.getElementById('cookie');
 const scoreDisplay = document.getElementById('score');
+const upgradeButton = document.getElementById("upgrade");
 
-cookie.onclick  = () => {
-    score++;
+cookie.onclick = () => {
+    score += cookiesPerClick;
     scoreDisplay.textContent = score;
+};
+
+upgradeButton.onclick = () => {
+    if (score >= upgradeCost) {
+        score -= upgradeCost;
+        cookiesPerClick += 2;
+        upgradeCost += 50;
+        scoreDisplay.textContent = score;
+        upgradeButton.textContent = `Upgrade: ${upgradeCost}`;
+    }
 };
